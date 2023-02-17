b5ffc7edc93a6871e6299a65fc25c79a912f7db7
Afiniki <jasminearumashel@gmail.com>
2023-02-17 08:37:04 -0800

    no e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
 
 /**
- *
- * main - start point 
+ * main - start point
  *
  * Return: always 0
  */
-
 int main(void)
 {
 	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
-		putchar(i);
-	for (i = 'A' ; i <= 'Z' ; i++)
-		putchar(i);
+		if (i != 'q' && i != 'e')
+			putchar(i);
 	putchar('\n');
 	return (0);
 }
