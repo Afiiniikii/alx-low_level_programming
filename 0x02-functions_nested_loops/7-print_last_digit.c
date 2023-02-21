d842e8a327667bc9c7c87727607803a4dc7081d4
Afiniki <jasminearumashel@gmail.com>
2023-02-21 15:19:38 -0800

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit function
+ *
+ * @i: function parameter
+ *
+ * Return: k
+ */
+
+int print_last_digit(int i)
+{
+	int k;
+
+	k = i % 10;
+	if (i < 0)
+		k = -i;
+	return (k);
+}
