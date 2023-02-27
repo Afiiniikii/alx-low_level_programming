902b457fcedc706a1e506e756642a6e05456f702
Afiniki <jasminearumashel@gmail.com>
2023-02-27 14:24:35 -0800

    program that updates value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updating value n to 98
+ * @n: integer point to variable to be updated
+ *
+ * Return: zero always
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
