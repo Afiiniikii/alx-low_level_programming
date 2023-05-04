7f65c3eedc8b193397cf70b73956cc4fcddfef78
Afiniki <jasminearumashel@gmail.com>
2023-05-04 15:44:17 -0700

    function that checks the endianness

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,28 +4,22 @@
  * binary_to_uint - converting binary number to a uint
  * @b: pointer to a character array
  * Description: convert  bin to uint
- * Return: binary number
+ * Return: value
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num = 0;
-
-	if (b == NULL)
-	{
-		return (0);
-	}
-
-	for (; *b != '\0'; b++)
+	unsigned int value = 0;
+		if (b == NULL)
+			return (0);
+	while (*b)
 	{
 		if (*b != '0' && *b != '1')
-		{
 			return (0);
-		}
 
-		num = num << 1;
-		num += (*b - '0');
+		value <<= 1;
+		if (*b++ == '1')
+			value += 1;
 	}
-
-	return (num);
+	return (value);
 }
