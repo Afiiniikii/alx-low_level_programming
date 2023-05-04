c5746de6096dbf8b3633034293d8443aac5bf5af
Afiniki <jasminearumashel@gmail.com>
2023-05-04 15:47:42 -0700

    a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,8 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char character);
 int get_bit(unsigned long int n, unsigned int index);
